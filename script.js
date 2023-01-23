63bcc2ef7857ecd7b8d4b78a2f7e7197c3cb73fd
Kevin Choi <kchoi@totalwine.com>
2023-01-23 16:46:39 -0500

    TT-18

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Engineering Training C2")
+const modalButton = document.getElementById('modalButton')
+const modalContainer = document.getElementById('modalContainer')
+
+console.log('modalButton', modalButton)
+
+modalButton.addEventListener('click', function(){
+    console.log("clicked button!")
+    modalContainer.classList.toggle('hidden')
+});
+
