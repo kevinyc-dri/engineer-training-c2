0cc7652a10977628a0ff292630833fc648146cb7
Kevin Choi <kchoi@totalwine.com>
2023-01-30 11:17:35 -0500

    tt-16

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("Engineering Training C2")
 const modalButton = document.getElementById('modalButton')
 const modalContainer = document.getElementById('modalContainer')
+const closeModalButton = document.getElementsByClassName('close-modal-button', console.log("closeModalButton"))
 
 console.log('modalButton', modalButton)
 
